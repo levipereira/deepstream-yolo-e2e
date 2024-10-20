dce0ddeaccaa6a002e5ffe2509daa073bbe7e4b1
Andrei <andreifrancap@gmail.com>
2024-10-20 00:21:49 -0300

    Added option to change resolution and fps

--- python_apps/component/source_factory.py
+++ python_apps/component/source_factory.py
@@ -1,12 +1,12 @@
 import sys
-import gi
+import gi # type: ignore
 import os
 import configparser
-import subprocess
 from component.yt_factory import get_yt_uri
+from common.constants import *
 
 gi.require_version('Gst', '1.0')
-from gi.repository import Gst
+from gi.repository import Gst # type: ignore
 
 
 def cb_newpad(decodebin, decoder_src_pad,data):
@@ -56,7 +56,6 @@ def create_source_bin(index,uri):
         sys.stderr.write(" Unable to create source bin \n")
 
     uri_decode_bin=Gst.ElementFactory.make("uridecodebin", "uri-decode-bin")
-        
     if not uri_decode_bin:
         sys.stderr.write(" Unable to create uri decode bin \n")
     
@@ -71,25 +70,87 @@ def create_source_bin(index,uri):
         return None
     return nbin
 
-
-def parse_media_source(config_file):
-    config = configparser.ConfigParser()
-    config.read(config_file)
-
+def parse_media_source(media_config_file, resolution=1080):
+    # Read media configuration
+    media_config = configparser.ConfigParser()
+    media_config.read(media_config_file)
+    
+    # Read streammux configuration
+    streammux_config = configparser.ConfigParser()
+    streammux_config.read(CONFIG_NEW_STREAMMUX_PATH)
+    
     media_entries = []
-
-    for section in config.sections():
-        enable = config.getint(section, 'enable')
+    source_count = 0
+    max_fps = 0
+    
+    for section in media_config.sections():
+        enable = media_config.getint(section, 'enable', fallback=0)
         if enable == 1:
-            type = config.get(section, 'type')
-            url = config.get(section, 'url')
-            uri = config.get(section, 'url')
-
+            type = media_config.get(section, 'type')
+            url = media_config.get(section, 'url')
+            uri = url
+            resolution = media_config.getint(section, 'resolution', fallback=1080)
+            fps = media_config.getfloat(section, 'fps', fallback=30.0)
+            
             if not os.path.isfile(url) and type == 'file':
                 print(f"File not found: {url}")
             if type == "file":
                 uri = f"file://{url}"
             if type == 'youtube':
-                uri = get_yt_uri(url)
-            media_entries.append((type,url,uri))
+                uri = get_yt_uri(url, resolution, fps)
+            media_entries.append((type, url, uri))
+            
+            # Update or add source-config section in streammux config
+            source_section = f'source-config-{source_count}'
+            if not streammux_config.has_section(source_section):
+                streammux_config.add_section(source_section)
+            
+            # Update max-fps-n and max-fps-d
+            streammux_config[source_section]['max-fps-n'] = str(int(fps))
+            streammux_config[source_section]['max-fps-d'] = '1'
+            
+            # Update max_fps if this source has a higher fps
+            max_fps = max(max_fps, fps)
+            
+            source_count += 1
+    
+    # Update overall-max-fps-n in the [property] section if it exists
+    if 'property' in streammux_config:
+        streammux_config['property']['overall-max-fps-n'] = str(int(max_fps))
+    
+    # Write the updated configuration back to the streammux config file
+    with open(CONFIG_NEW_STREAMMUX_PATH, 'w') as configfile:
+        streammux_config.write(configfile)
+    
     return media_entries
+
+def float_to_fraction(x, error=0.000001):
+    n = int(x)
+    x -= n
+    if x < error:
+        return n, 1
+    elif 1 - error < x:
+        return n+1, 1
+    
+    # The lower fraction is 0/1
+    lower_n, lower_d = 0, 1
+    # The upper fraction is 1/1
+    upper_n, upper_d = 1, 1
+    while True:
+        # The middle fraction is (lower_n + upper_n) / (lower_d + upper_d)
+        middle_n = lower_n + upper_n
+        middle_d = lower_d + upper_d
+        # If x + error < middle
+        if middle_d * (x + error) < middle_n:
+            # middle is our new upper
+            upper_n, upper_d = middle_n, middle_d
+        # Else If middle < x - error
+        elif middle_n < (x - error) * middle_d:
+            # middle is our new lower
+            lower_n, lower_d = middle_n, middle_d
+        # Else middle is our best fraction
+        else:
+            return n * middle_d + middle_n, middle_d
+
+# Example usage:
+# parse_media_source('media.ini', 'new_streammux_config.txt')
